bb3b264f11517799a4eb1a8ee5812c695296eee8
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2025-10-15 12:11:36 +0800

    fix: R8 failed

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "../proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         debug {
             isMinifyEnabled = false
