2809743f944f0c32b8ba3875a8306ff70874941a
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2025-10-16 11:20:51 +0800

    misc: cleanup code

--- library/src/commonMain/kotlin/project/pipepipe/app/viewmodel/EmptyViewModel.kt
+++
@@ -1,7 +0,0 @@
-//package project.pipepipe.shared.viewmodel
-//
-//import project.pipepipe.shared.uistate.EmptyUiState
-//
-//class EmptyViewModel(val showMessage: Boolean) : BaseViewModel<EmptyUiState>(
-//    EmptyUiState(showMessage = showMessage)
-//)
