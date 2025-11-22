a2bcde261825173f09ad1bd533e1c6fc49080df2
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2025-11-22 15:08:31 +0800

    fix: 10+ years shown as xx decades

--- library/src/androidMain/kotlin/project/pipepipe/app/utils/TimeHelper.android.kt
+++ library/src/androidMain/kotlin/project/pipepipe/app/utils/TimeHelper.android.kt
@@ -2,8 +2,16 @@ package project.pipepipe.app.utils
 
 
 import org.ocpsoft.prettytime.PrettyTime
+import org.ocpsoft.prettytime.units.Decade
 import java.util.Date
 
+private val prettyTime: PrettyTime by lazy {
+    PrettyTime().apply {
+        // Do not use decades as YouTube doesn't either.
+        removeUnit(Decade::class.java)
+    }
+}
+
 actual fun formatRelativeTime(timestampMillis: Long): String {
-    return PrettyTime().format(Date(timestampMillis))
+    return prettyTime.format(Date(timestampMillis))
 }
